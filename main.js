e6ba1eeba3e180bff865cdec3724d2e0c23c1251
balqeesalkhatib <Balqees.alkhateb@gmail.com>
2024-02-14 17:46:01 +0300

    loading

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ home.append(pic1, pic2, pic3, mode);
 mode.append(dark, light);
 dark.on("click", function () {
   body.removeClass("lightMode").addClass("darkMode");
-  $('.main').css('background-color','black')
+  $(".main").css("background-color", "black");
 });
 light.on("click", function () {
   body.removeClass("darkMode").addClass("lightMode");
@@ -56,15 +56,18 @@ const womenProducts = [
   {
     id: 1,
     title: "Winter Jacket",
-    imageSrc: "https://cdn.pixabay.com/photo/2016/08/04/18/30/model-1569741_640.jpg",
-    description: "Beautiful lady overjoyed by warm spring breeze dream of romantic date wear cute floral dress isolated pink background stock photo",
+    imageSrc:
+      "https://cdn.pixabay.com/photo/2016/08/04/18/30/model-1569741_640.jpg",
+    description:
+      "Beautiful lady overjoyed by warm spring breeze dream of romantic date wear cute floral dress isolated pink background stock photo",
     rate: 10,
     price: 25,
   },
   {
     id: 2,
     title: "Traditional dress",
-    imageSrc: "https://cdn.pixabay.com/photo/2014/02/27/16/10/medieval-276019_640.jpg",
+    imageSrc:
+      "https://cdn.pixabay.com/photo/2014/02/27/16/10/medieval-276019_640.jpg",
     description: "some description about product",
     rate: 10,
     price: 25,
@@ -72,7 +75,8 @@ const womenProducts = [
   {
     id: 3,
     title: " traditional Wedding dress",
-    imageSrc: "https://cdn.pixabay.com/photo/2021/02/11/05/34/woman-6004282_640.jpg",
+    imageSrc:
+      "https://cdn.pixabay.com/photo/2021/02/11/05/34/woman-6004282_640.jpg",
     description: "some description about product",
     rate: 10,
     price: 25,
@@ -80,7 +84,8 @@ const womenProducts = [
   {
     id: 4,
     title: "Indian Sari",
-    imageSrc: "https://cdn.pixabay.com/photo/2014/05/30/15/01/sari-358314_640.jpg",
+    imageSrc:
+      "https://cdn.pixabay.com/photo/2014/05/30/15/01/sari-358314_640.jpg",
     description: "some description about product",
     rate: 10,
     price: 25,
@@ -92,12 +97,28 @@ womenHome.hide();
 const homeReturn = $(`<div class='homeReturn'>back to home</div>`);
 const products = $(`<div class="products"><b>Women Section</b></div>`);
 womenHome.append(homeReturn, products);
-const womenDescription = $(`<div class="womenDescription">womenDescription</div>`);
-body.append(womenDescription)
-womenDescription.hide()
-const divImg=$(`<img  src=''>`)
-const divDescription=$(`<div class="divDescription">divDescription</div>`)
-womenDescription.append(divImg,divDescription)
+const womenDescription = $(`<div class="womenDescription"></div>`);
+body.append(womenDescription);
+womenDescription.hide();
+const divBack = $(`<div class='divBack'></div>`);
+const homeReturn4 = $(`<div class='homeReturn'>back to home</div>`);
+const backWomen = $(`<div class='backWomen'>Back</div>`);
+//I should add cart and fav
+const divImg = $(`<img  src=''>`);
+const divTitle = $(`<div class="divTitle"></div>`);
+const divDescription = $(`<div class="divDescription"></div>`);
+divBack.append(backWomen, homeReturn4);
+womenDescription.append(divBack, divTitle, divImg, divDescription);
+backWomen.on("click", function () {
+  womenHome.show();
+  womenDescription.hide();
+});
+homeReturn4.on("click", function () {
+  home.show();
+  womenHome.hide();
+  menHome.hide();
+  womenDescription.hide();
+});
 homeReturn.on("click", function () {
   home.show();
   womenHome.hide();
@@ -109,16 +130,15 @@ const render = () => {
       `<div class="divWomen"><img src='${elem.imageSrc}'alt='${elem.title}'><h4>${elem.title}</h4> </div>`
     );
     womenHome.append(divWomen);
-    divWomen.on('click',function () {
-        //============description==========
-        home.hide();
-        womenHome.hide();
-        womenDescription.show()
-        console.log(this);
-        console.log(`${elem.imageSrc}`);
-        divImg.attr("src", `${elem.imageSrc}`);
-        // divImg.append(elem.imageSrc)
-    })
+    divWomen.on("click", function () {
+      //============description==========
+      home.hide();
+      womenHome.hide();
+      womenDescription.show();
+      divImg.attr("src", `${elem.imageSrc}`);
+      divTitle.text(`${elem.title}`);
+      divDescription.text(`${elem.description}`);
+    });
   });
 };
 render();
@@ -133,7 +153,8 @@ const menProducts = [
   {
     id: 1,
     title: "Suit fashion",
-    imageSrc: "https://cdn.pixabay.com/photo/2018/02/07/19/46/suit-3137825_640.png",
+    imageSrc:
+      "https://cdn.pixabay.com/photo/2018/02/07/19/46/suit-3137825_640.png",
     description: "some description about product",
     rate: 10,
     price: 25,
@@ -141,7 +162,8 @@ const menProducts = [
   {
     id: 2,
     title: "suit menswear",
-    imageSrc: "https://cdn.pixabay.com/photo/2015/04/09/11/19/suit-714357_640.jpg",
+    imageSrc:
+      "https://cdn.pixabay.com/photo/2015/04/09/11/19/suit-714357_640.jpg",
     description: "some description about product",
     rate: 10,
     price: 25,
@@ -149,7 +171,8 @@ const menProducts = [
   {
     id: 3,
     title: "cotton shirt",
-    imageSrc: "https://cdn.pixabay.com/photo/2016/11/29/10/21/jacket-1868990_640.jpg",
+    imageSrc:
+      "https://cdn.pixabay.com/photo/2016/11/29/10/21/jacket-1868990_640.jpg",
     description: "some description about product",
     rate: 10,
     price: 25,
@@ -157,7 +180,8 @@ const menProducts = [
   {
     id: 4,
     title: "Suit",
-    imageSrc: "https://cdn.pixabay.com/photo/2019/06/13/12/07/suit-4271440_640.jpg",
+    imageSrc:
+      "https://cdn.pixabay.com/photo/2019/06/13/12/07/suit-4271440_640.jpg",
     description: "some description about product",
     rate: 10,
     price: 25,
@@ -195,34 +219,38 @@ const kidsProducts = [
   {
     id: 1,
     title: "Newborn baby clothes",
-    imageSrc: "https://cdn.pixabay.com/photo/2017/02/08/02/56/booties-2047596_640.jpg",
+    imageSrc:
+      "https://cdn.pixabay.com/photo/2017/02/08/02/56/booties-2047596_640.jpg",
     description: "some description about product",
     rate: 10,
-    price: '25JD',
+    price: "25JD",
   },
   {
     id: 2,
     title: "Kids Shirt",
-    imageSrc: "https://cdn.pixabay.com/photo/2018/03/02/10/17/portrait-3192816_1280.jpg",
+    imageSrc:
+      "https://cdn.pixabay.com/photo/2018/03/02/10/17/portrait-3192816_1280.jpg",
     description: "some description about product",
     rate: 10,
-    price: '7JD',
+    price: "7JD",
   },
   {
     id: 3,
     title: "Traditional clothing",
-    imageSrc: "https://cdn.pixabay.com/photo/2015/05/20/14/27/traditional-775512_640.jpg",
+    imageSrc:
+      "https://cdn.pixabay.com/photo/2015/05/20/14/27/traditional-775512_640.jpg",
     description: "some description about product",
     rate: 10,
-    price:'35JD',
+    price: "35JD",
   },
   {
     id: 4,
     title: "Socks",
-    imageSrc: "https://cdn.pixabay.com/photo/2016/08/08/16/11/ankle-socks-1578652_640.jpg",
+    imageSrc:
+      "https://cdn.pixabay.com/photo/2016/08/08/16/11/ankle-socks-1578652_640.jpg",
     description: "some description about product",
     rate: 10,
-    price: '2JD',
+    price: "2JD",
   },
 ];
 const kidsHome = $(`<div class="kidsHome"></div>`);
@@ -256,7 +284,7 @@ const nameAccount =
   $(`<div class="nameAccount" style="font-size: xx-large;">Create Account</div>
 <label> Email:</label> <br><input placeholder="Enter your email" type="email"><br><label> Password:</label> <br><input placeholder="Enter your password" type="password"><br><label> Confirm password:</label> <br><input placeholder="Confirm your password" type="password"><br>I agree<input type="radio" required><br>`);
 const register = $(` <button>Register</button>`);
-createAccount.append( nameAccount, register,homeReturn3);
+createAccount.append(nameAccount, register, homeReturn3);
 homeReturn3.on("click", function () {
   home.show();
   createAccount.hide();
