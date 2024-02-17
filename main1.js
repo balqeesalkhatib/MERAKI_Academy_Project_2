dac1ab4e02d67af8401d0e5cad105ee67f92f7d2
balqeesalkhatib <Balqees.alkhateb@gmail.com>
2024-02-17 15:34:42 +0300

    search

--- main1.js
+++ main1.js
@@ -12,27 +12,152 @@ const mainContains =
 <div class='cart' ><i style='font-size:xx-large' class="fa-solid fa-cart-shopping"></i></div>
 <div class='fav' style='font-size:xx-large'><i class="fa-solid fa-heart"></i></div>`);
 main.append(mainContains);
-// $("#searchInput").on("click", function () {
-//   if ($("#searchInput").val() === "Women".toLowerCase()) {
-//     home.hide();
-//     womenHome.show();
-//   } else if ($("#searchInput").val() === "MEN".toLowerCase()) {
-//     home.hide();
-//     home.hide();
-//     menHome.show();
-//   } else if ($("#searchInput").val() === "kids".toLowerCase()) {
-//     home.hide();
-//     kidsHome.show();
-//   } else {
-//     $("#searchInput").val("");
-//   }
-// });
+$("#searchInput").on("click", function () {
+  if ($("#searchInput").val() === "Women".toLowerCase()) {
+    home.hide();
+    categoryHome.show();
+    render([
+        {
+          id: 1,
+          title: "Winter Jacket",
+          imageSrc:
+            "https://cdn.pixabay.com/photo/2016/08/04/18/30/model-1569741_640.jpg",
+          description:
+            "Beautiful lady overjoyed by warm spring breeze dream of romantic date wear cute floral dress isolated pink background stock photo",
+          rate: 8.5,
+          price: "33 JD",
+        },
+        {
+          id: 2,
+          title: "Traditional dress",
+          imageSrc:
+            "https://cdn.pixabay.com/photo/2014/02/27/16/10/medieval-276019_640.jpg",
+          description:
+            "Using the word sophisticated can convey a sense of class and elegance that is attractive and timeless. Describing outfits and women's dresses can be a challenge, but using creative",
+          rate: 10,
+          price: "45 JD",
+        },
+        {
+          id: 3,
+          title: " traditional Wedding dress",
+          imageSrc:
+            "https://cdn.pixabay.com/photo/2021/02/11/05/34/woman-6004282_640.jpg",
+          description:
+            "fashion is the most general term and applies to any way of dressing, behaving, writing, or performing that is favored at any one time or place",
+          rate: 9.5,
+          price: "30 JD",
+        },
+        {
+          id: 4,
+          title: "Indian Sari",
+          imageSrc:
+            "https://cdn.pixabay.com/photo/2014/05/30/15/01/sari-358314_640.jpg",
+          description:
+            " dress (also known as a frock or a gown) is a garment traditionally worn by women or girls consisting of a skirt with an attached bodice (or a matching bodice giving the effect of a one-piece garment)",
+          rate: 10,
+          price: "50 JD",
+        },
+      ])
+  } else if ($("#searchInput").val() === "MEN".toLowerCase()) {
+    home.hide();
+    categoryHome.show();
+    render([
+        {
+          id: 1,
+          title: "Suit fashion",
+          imageSrc:
+            "https://cdn.pixabay.com/photo/2018/02/07/19/46/suit-3137825_640.png",
+          description:
+            "suit, lounge suit, or business suit is a set of clothes comprising a suit jacket and trousers of identical textiles generally worn with a collared dress shirt, necktie, and dress shoes.",
+          rate: 8.5,
+          price: "25JD",
+      
+        },
+        {
+          id: 2,
+          title: "suit menswear",
+          imageSrc:
+            "https://cdn.pixabay.com/photo/2015/04/09/11/19/suit-714357_640.jpg",
+          description:
+            "The most important quality of a good suit is a jacket that has a full canvas layer between the fabric and lining.",
+          rate: 9,
+          price: "35 JD",
+        },
+        {
+          id: 3,
+          title: "cotton shirt",
+          imageSrc:
+            "https://cdn.pixabay.com/photo/2016/11/29/10/21/jacket-1868990_640.jpg",
+          description:
+            "The most important quality of a good suit is a jacket that has a full canvas layer between the fabric and lining.",
+          rate: 9.5,
+          price: "15JD",
+        },
+        {
+          id: 4,
+          title: "Suit",
+          imageSrc:
+            "https://cdn.pixabay.com/photo/2019/06/13/12/07/suit-4271440_640.jpg",
+          description:
+            " Wearing a suit signifies that you are taking the other person more seriously and are striving to stand up to their expectations",
+          rate: 10,
+          price: "75 JD",
+        },
+      ])
+  } else if ($("#searchInput").val() === "kids".toLowerCase()) {
+    console.log('gg');
+    home.hide();
+    categoryHome.show();
+    render([
+        {
+          id: 1,
+          title: "Newborn baby clothes",
+          imageSrc:
+            "https://cdn.pixabay.com/photo/2017/02/08/02/56/booties-2047596_640.jpg",
+          description:
+            "comfortable, soft and easy to take care of. Stretchy jumpsuits that fasten at the front are best, as well as tops with envelope necks, which are easier to get over your baby's head. Jumpsuits with zips can make dressing your baby quick and easy too.",
+          rate: 9.5,
+          price: "25JD",
+        },
+        {
+          id: 2,
+          title: "Kids Shirt",
+          imageSrc:
+            "https://cdn.pixabay.com/photo/2018/03/02/10/17/portrait-3192816_1280.jpg",
+          description:
+            "a lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment",
+          rate: 10,
+          price: "7JD",
+        },
+        {
+          id: 3,
+          title: "Traditional clothing",
+          imageSrc:
+            "https://cdn.pixabay.com/photo/2015/05/20/14/27/traditional-775512_640.jpg",
+          description:
+            "one of the more important attributes to look for in infant and baby clothing is that the clothes are soft and not rough. Soft baby clothes made from organic cotton or eco-friendly materials are becoming more popular.",
+          rate: 8.5,
+          price: "35JD",
+        },
+        {
+          id: 4,
+          title: "Socks",
+          imageSrc:
+            "https://cdn.pixabay.com/photo/2016/08/08/16/11/ankle-socks-1578652_640.jpg",
+          description:
+            "Comfort and Skin Sensitivity: Babies have delicate and sensitive skin, making it crucial to prioritize their comfort. ",
+          rate: 9,
+          price: "2JD",
+        },
+      ])
+  } else {
+    $("#searchInput").val("");
+  }
+});
 $(".account").on("click", function () {
+    console.log('k');
   home.hide();
-  womenHome.hide();
-  menHome.hide();
-  kidsHome.hide();
-  createAccount.show();
+   createAccount.show();
 });
 const category = $(`<div class='category'></div>`);
 home.append(category);
@@ -326,6 +451,7 @@ const renderCart = (cartArr) => {
   })
 };
 const render = (newArray) => {
+
   categoryHome.html("");
   categoryHome.append(homeReturn, products);
   newArray.forEach((e, indx) => {
@@ -418,13 +544,8 @@ const renderOneItem = function (e) {
 
 $(".cart").on("click", function () {
   home.hide();
-  //   womenHome.hide();
-  //   menHome.hide();
-  //   kidsHome.hide();
-  //   createAccount.hide();
-  console.log(cartArr);
-  //   $('.createCart').show();
-  categoryHome.show();
+   console.log(cartArr);
+   categoryHome.show();
   //   render(cartArr);
   renderCart(cartArr);
 });
@@ -433,4 +554,20 @@ $(".fav").on("click", function () {
   categoryHome.show();
   renderFav(favArr);
 });
+//===================
+const createAccount = $(`<div class="createAccount"></div>`);
+body.append(createAccount);
+createAccount.hide();
+const homeReturn3 = $(
+  `<div class='homeReturn3'><i class="fa-solid fa-house"></i></div>`
+);
+const nameAccount =
+  $(`<div class="nameAccount" style="font-size: xx-large;">Create Account</div>
+<label> Email:</label> <br><input placeholder="Enter your email" type="email"><br><label> Password:</label> <br><input placeholder="Enter your password" type="password"><br><label> Confirm password:</label> <br><input placeholder="Confirm your password" type="password"><br>I agree<input type="radio" required><br>`);
+const register = $(` <button>Register</button>`);
+createAccount.append(homeReturn3,nameAccount, register );
+homeReturn3.on("click", function () {
+    home.show();
+    createAccount.hide();
+      });
 // render()
